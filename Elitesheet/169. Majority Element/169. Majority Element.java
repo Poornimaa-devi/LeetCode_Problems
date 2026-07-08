c8bf7b787016d242ba9f6a5c29f848f986d30797
Poornimaa-devi <poornimaadevi4342@gmail.com>
2026-07-08 10:15:39 +0530

    [169] [Time Beats: 70.19%] [Memory Beats: 6.56%] - Leet2Hub

---
+++ Elitesheet/169. Majority Element/169. Majority Element.java
@@ -0,0 +1,15 @@
+1class Solution {
+2    public int majorityElement(int[] nums) {
+3        int candidate = nums[0];
+4        int count = 0;
+5        
+6        for (int num : nums) {
+7            if (count == 0) {
+8                candidate = num;
+9            }
+10            count += (num == candidate) ? 1 : -1;
+11        }
+12        
+13        return candidate;
+14    }
+15}
