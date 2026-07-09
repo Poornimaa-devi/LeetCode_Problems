839c0f9c7ece46d38ad51385843ac86e41333724
Poornimaa-devi <poornimaadevi4342@gmail.com>
2026-07-09 10:01:02 +0530

    [53] [Time Beats: N/A] [Memory Beats: N/A] - Leet2Hub

---
+++ Elitesheet/53. Maximum Subarray/53. Maximum Subarray.java
@@ -0,0 +1,1 @@
+class Solution {    public int maxSubArray(int[] nums) {        int curr=0;        int max = nums[0];        for(int i=0;i<nums.length;i++){            curr=Math.max(curr,0);            curr+=nums[i];        }    }            max=Math.max(max,curr);        return max;}
