42cd2a1849371bc3170b64aeca926193eae471d6
Poornimaa-devi <poornimaadevi4342@gmail.com>
2026-08-15 10:37:41 +0530

    [0000] [Time Beats: N/A] [Memory Beats: N/A] - Leet2Hub

--- Zoho_Prev_Problems/0000. Longest Subsequence With Non Zero Bitwise Xor/0000. Longest Subsequence With Non Zero Bitwise Xor.java
+++ Zoho_Prev_Problems/0000. Longest Subsequence With Non Zero Bitwise Xor/0000. Longest Subsequence With Non Zero Bitwise Xor.java
@@ -1,14 +1,1 @@
-1class Solution {
-2    public int longestSubsequence(int[] nums) {
-3        int result = 0;
-4        boolean hasnonzero = false;
-5        for(int i=0;i<nums.length;i++){
-6            if(nums[i]!=0) hasnonzero = true;
-7            result^=nums[i];
-8
-9        }
-10        if(!hasnonzero) return 0;
-11        if(result>0) return nums.length;
-12        else return nums.length-1;
-13    }
-14}
+            result^=nums[i];        }        if(!hasnonzero) return 0;        if(result>0) return nums.length;        else return nums.length-1;            if(nums[i]!=0) hasnonzero = true;class Solution {    public int longestSubsequence(int[] nums) {        int result = 0;        boolean hasnonzero = false;        for(int i=0;i<nums.length;i++){
