61144d57705f3f928a5cfe4103a02b21bc58fc29
Poornimaa-devi <poornimaadevi4342@gmail.com>
2026-09-03 10:58:26 +0530

    [3876] [Time Beats: 34%] [Memory Beats: 83.33%] - Leet2Hub

---
+++ Zoho_Prev_Problems/3876. Construct Uniform Parity Array II/3876. Construct Uniform Parity Array II.java
@@ -0,0 +1,11 @@
+1class Solution {
+2    public boolean uniformArray(int[] nums1) {
+3        int mn = Integer.MAX_VALUE;
+4        int count = 0;
+5        for (int x : nums1) {
+6            mn = Math.min(mn, x);
+7            if (x % 2 == 1) count++;
+8        }
+9        return mn % 2 == 1 || count == 0;
+10    }
+11}
