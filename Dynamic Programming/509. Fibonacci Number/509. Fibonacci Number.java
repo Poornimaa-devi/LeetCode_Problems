76e062f435695855ed086d5e4d92c6a5852e5af9
Poornimaa-devi <poornimaadevi4342@gmail.com>
2026-09-22 10:11:09 +0530

    [509] [Time Beats: N/A] [Memory Beats: N/A] - Leet2Hub

---
+++ Dynamic Programming/509. Fibonacci Number/509. Fibonacci Number.java
@@ -0,0 +1,1 @@
+    static int solve(int n,int[] dp){         if(n==0 || n==1){            return n;         }         if(dp[n]!=-1){            return dp[n];         }    public int fib(int n) {        int [] dp = new int[n+1];        Arrays.fill(dp,-1);        int res=solve(n,dp);        return res;    }class Solution {
